ec4df35d6c38fa7c28d1d1cb49206fd3ed1284ca
Akbar Odilov <akbar@odilov.de>
2021-09-22 15:47:28 +0200

    Add DbContext & Repo

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using PlatformService.Models;
+
+namespace PlatformService.Data{
+    public interface IPlatformRepo{
+        bool SaveChanges();
+        IEnumerable <Platform> GetPlatforms();
+        Platform GetPlatformById( int ID );
+        void CreatePlatform( Platform platform );
+    }
+}
