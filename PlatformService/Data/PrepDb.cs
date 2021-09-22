53485b5ad7180ec8b4d55fd56d8970f158e10d1e
Akbar Odilov <akbar@odilov.de>
2021-09-22 16:28:07 +0200

    add test data & dtos

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data{
+    public static class PrepDb{
+        public static void PrepPopulation( IApplicationBuilder app ){
+            using ( var serviceScope = app.ApplicationServices.CreateScope(  ) ){
+                SeedData( serviceScope.ServiceProvider.GetService< AppDbContext >() );
+            }
+        }
+        private static void SeedData( AppDbContext context ){
+            if( !context.Platforms.Any() ){
+                Console.WriteLine( "--> seeding data" );
+                context.AddRange( new Platform(){
+                    Name = "Dot Net",
+                    Publisher = "Microsoft",
+                    Cost = "Free"
+                } );
+                context.SaveChanges();
+            } else {
+                Console.WriteLine( "--> data already exists" );
+            }
+        }
+    }
+}
--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -57,6 +57,9 @@ namespace PlatformService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.PrepPopulation( app );
+
         }
     }
 }
