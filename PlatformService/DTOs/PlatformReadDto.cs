329031face3ad754b7d68c39d0f86c84e5324f87
Akbar Odilov <akbar@odilov.de>
2021-09-22 16:27:17 +0200

    add test data & dtos

---
+++ PlatformService/DTOs/PlatformReadDto.cs
@@ -0,0 +1,8 @@
+namespace PlatformService.DTOs{
+    public class PlatformReadDto{
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
