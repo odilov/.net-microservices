f1813cf16adac8d6555f3b1fa87a67ab35d99f98
Akbar Odilov <akbar@odilov.de>
2021-09-22 17:03:31 +0200

    Add AutoMapper

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles{
+    public class PlatformsProfile : Profile {
+        public PlatformsProfile(){
+            // source -> target
+            CreateMap <Platform, PlatformReadDto>();
+            CreateMap <PlatformCreateDto, Platform>();
+        }
+    }
+}
